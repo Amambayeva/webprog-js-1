8f2d5c6ed4cb8a9d8a7d7c8ba9345a067c83b012
Amambayeva <80603455+Amambayeva@users.noreply.github.com>
2022-06-07 23:26:19 +0200

    Add files via upload
    updated

--- constants.js
+++ constants.js
@@ -1,10 +1,34 @@
 const RESULTS_MAP = {
   'one': {
     correct: 2,
-    hint: "Text 1",
+    hint: "The <title> element specifies a title for the HTML page, which is shown in the browser's title bar or in the page's tab",
   },
   'two': {
-    correct: 2,
-    hint: "Text 2",
+    correct: 3,
+    hint: "",
+  },
+  'three': {
+    correct: 1,
+    hint: "Transmission Control Protocol (TCP) is a standard that defines how to establish and maintain a network conversation by which applications can exchange data. TCP works with the Internet Protocol (IP), which defines how computers send packets of data to each other.",
+  },
+  'four': {
+    correct: 3,
+    hint: "",
+  },
+  'five': {
+    correct: 1,
+    hint: "",
+  },
+  'six': {
+    correct: 1,
+    hint: "",
+  },
+  'seven': {
+    correct: 1,
+    hint: "",
+  },
+  'eight': {
+    correct: 1,
+    hint: "",
   }
 };
--- script.js
+++ script.js
@@ -1,4 +1,74 @@
-// TODO(you): Write the JavaScript necessary to complete the homework.
 
-// You can access the RESULTS_MAP from "constants.js" in this file since
-// "constants.js" has been included before "script.js" in index.html.
+/*Answer staff*/
+function chooseOption(event) {
+
+  let label = "wrong"; //default
+  let hintLabel = "Wrong!"; //default
+
+  const choice = event.currentTarget;
+  const questionId = choice.parentNode.dataset.questionId;
+  const choiceGrid = choice.parentNode;
+  const choiceId = choice.dataset.choiceId;
+  const correctChoiceId = RESULTS_MAP[questionId].correct;
+
+
+  if (correctChoiceId == choiceId) {
+    label = "correct";
+    hintLabel = "Correct!";
+  }
+
+  choice.classList.add(label);
+
+/*Answer staff decided to put Right or Wrong*/
+
+  let correctAnswerText = "";
+
+  for (let i = availableOptions.length - 1; i >= 0; i--) {
+    let option = availableOptions[i];
+
+    if (option.parentNode.dataset.questionId == questionId) {
+      if (option.dataset.choiceId == correctChoiceId) {
+        option.classList.add("correct");
+      }
+
+      option.classList.add("finished");
+      option.removeEventListener('click', chooseOption);
+      const deleteIndex = availableOptions.indexOf(option);
+      availableOptions.splice(deleteIndex, 1);
+    }
+  }
+
+  addHint(choiceGrid, questionId, label, hintLabel, correctAnswerText);
+}
+
+
+const options = document.querySelectorAll('.choice-grid div');
+const availableOptions = [];
+for (const option of options) {
+  option.addEventListener('click', chooseOption);
+  availableOptions.push(option);
+}
+
+
+/*Hints staff*/
+function addHint(choiceGrid, questionId, label, hintLabel, correctAnswerText)
+{
+  const hint = document.createElement('section');
+  hint.classList.add("hints");
+
+  const hintHeader = document.createElement('div'); //add label
+  hintHeader.classList.add(label);
+  hintHeader.textContent = hintLabel;
+  hint.appendChild(hintHeader);
+
+  const correctAnswer = document.createElement('b'); //add correct answer
+  correctAnswer.textContent = correctAnswerText;
+  hint.appendChild(correctAnswer);
+
+  const hintText = document.createElement('p'); //add explanation
+  hintText.textContent = RESULTS_MAP[questionId].hint;
+  hint.appendChild(hintText);
+
+  hint.classList.add(label);
+  choiceGrid.parentNode.insertBefore(hint, choiceGrid.nextSibling);
+}
